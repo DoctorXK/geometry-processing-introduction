acedb0721d1b38a039e6044fa4caaf9e92e015d4
XK Chen <cxk19971105@gmail.com>
2022-09-14 15:26:27 +0800

    test

--- src/edges.cpp
+++ src/edges.cpp
@@ -4,5 +4,6 @@ Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
   // ADD YOUR CODE HERE
+  // 陈向楷的代码
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -3,6 +3,7 @@
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
-  // ADD YOUR CODE HERE
+  // ADD YOUR CODE HERE.
+  // 陈向楷的代码！！！
   return Chi;
 }
